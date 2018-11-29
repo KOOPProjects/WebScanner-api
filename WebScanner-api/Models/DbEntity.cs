191f48ac16e0aa021d4dd9e04e6db733c5c26319
Oskar <32549612+OskarPrzybylski@users.noreply.github.com>
2018-11-29 22:39:02 +0100

    Added basic interface for all repositories, added generic implementation of it, added DbEntity (#3)

---
+++ WebScanner-api/Models/DbEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebScanner_api.Models
+{
+    public abstract class DbEntity
+    {
+        public int Id { get; set; }
+
+    }
+}
