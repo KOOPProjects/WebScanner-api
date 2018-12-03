30551a0d1b83b5929915c799d713cf3aaf54a2cd
Oliwia Zarychta <36681510+ozarychta@users.noreply.github.com>
2018-12-03 01:05:21 +0100

    Implement connection to database (#4)
    * implemented getting responses from database
    
    * searching by content ignoring lower/uppercases
    
    * updated README.md
    
    * updated CHANGELOG.md

---
+++ WebScanner-api/Models/Repositories/ResponseRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebScanner_api.Models.Database;
+using WebScanner_api.Models.Repositories.Interfaces;
+
+namespace WebScanner_api.Models.Repositories
+{
+    public class ResponseRepository : DbEntityRepository<Response>, IResponseRepository
+    {
+        public ResponseRepository(DatabaseContext databaseContext) : base(databaseContext)
+        {
+        }
+
+        public IEnumerable<Response> GetMany(int[] ids)
+        {
+            return this.DatabaseContext.Responses.Where(x => ids.Contains(x.OrderId));
+        }
+
+        public IEnumerable<Response> GetResponseByIdDateAndContent(int[] ids, DateTime dateAfter, DateTime dateBefore, string content)
+        {
+            return this.DatabaseContext.Responses.Where(
+                x =>  ids.Contains(x.OrderId)
+                    && (dateAfter.CompareTo(x.Date) <= 0)
+                    && (dateBefore.CompareTo(x.Date) >= 0)
+                    && x.Content.ToLower().Contains(content.ToLower())
+                );
+        }
+    }
+}
